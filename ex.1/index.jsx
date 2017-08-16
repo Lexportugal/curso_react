4bbf8ca72e43e49d6dce6c65e192937525574af3
Lexportugal <alex.gomes@ftd.com.br>
2017-08-15 21:18:04 -0300

    Att Repo do curso_react+redux

---
+++ ex.1/index.jsx
@@ -0,0 +1,4 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+ReactDOM.render(<h1>Entrou os html ALELUIA PORRA</h1>, document.getElementById('app'))
---
+++ ex.2/component.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+
+export default () => (
+    <h1>Primeiro Componente!</h1>
+)
